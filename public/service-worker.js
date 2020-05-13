60da2e97c0c553b6b240cb05118717d196ead80d
Anna Olt <anna.olt16@gmail.com>
2020-05-13 17:21:30 -0400

    changed filepath for icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+console.log(self);
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
